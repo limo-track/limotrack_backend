90354e735305873687698b18cd7b6dba485899cd
Tarek Morshed <Tarek.Morshid@gmail.com>
2021-10-21 13:48:24 +0200

    Moved firebase keys to .env

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,13 +2,13 @@ const firebase = require('firebase/app');
 require('firebase/firestore')
     // got from app settings on firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyB9xaxIpIXq7KdBxdBEkDiqGk6R2B7aLSE",
-    authDomain: "test-59372.firebaseapp.com",
-    projectId: "test-59372",
-    storageBucket: "test-59372.appspot.com",
-    messagingSenderId: "24501278822",
-    appId: "1:24501278822:web:f39c7254de0a8e48fdb5a7",
-    measurementId: "G-CB8QNDFKC4"
+    apiKey: process.env.FIREBASE_apiKey,
+    authDomain: process.env.FIREBASE_authDomain,
+    projectId: process.env.FIREBASE_projectId,
+    storageBucket: process.env.FIREBASE_storageBucket,
+    messagingSenderId: process.env.FIREBASE_messagingSenderId,
+    appId: process.env.FIREBASE_appId,
+    measurementId: process.env.FIREBASE_measurementId
 };
 
 // Initialize Firebase
